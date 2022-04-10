78117e11c3f60e96b876faa918062ebc92ce7044
Aaron Rothstein <aaron.rothstein@wunita.com>
2022-04-09 21:25:20 -0500

    Completed Rust setup module.

---
+++ hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\code\rust-learning-path\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\code\rust-learning-path\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+C:\code\rust-learning-path\hello-cargo\target\debug\hello-cargo.exe: C:\code\rust-learning-path\hello-cargo\src\main.rs
