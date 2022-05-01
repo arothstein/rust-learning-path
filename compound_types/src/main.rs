6b1907ab3913bf56f10bd75a078447abb2acd551
Aaron Rothstein <aaron.rothstein@wunita.com>
2022-05-01 17:48:56 -0500

    Complete car_factory exercise.

--- compound_types/src/main.rs
+++ compound_types/src/main.rs
@@ -3,24 +3,23 @@ fn main() {
     let colors = ["Blue", "Green", "Red", "Silver"];
 
     // Declare the car type and initial values
-    let mut car: Car;
     let mut engine: Transmission = Transmission::Manual;
 
     // Order 3 cars, one car for each type of transmission
 
     // Car order #1: New, Manual, Hard top
-    car = car_factory(String::from(colors[0]), engine, true, 0);
-    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {:?} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+    car_factory(String::from(colors[0]), engine, true, 0);
+    // println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {:?} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
 
     // Car order #2: Used, Semi-Automatic, Convertible
     engine = Transmission::SemiAuto;
-    car = car_factory(String::from(colors[1]), engine, false, 100);
-    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {:?} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+    car_factory(String::from(colors[1]), engine, false, 565);
+    // println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {:?} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
 
     // Car order #3: Used, Automatic, Hard top
     engine = Transmission::Automatic;
-    car = car_factory(String::from(colors[2]), engine, true, 200);
-    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {:?} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+    car_factory(String::from(colors[2]), engine, true, 3000);
+    // println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {:?} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
 }
 
 #[derive(PartialEq, Debug)]
@@ -48,7 +47,28 @@ enum Age {New, Used}
 // Call the car_quality(miles) function to get the car age
 // Return an instance of a "Car" struct with the arrow '->' syntax
 fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
-    // Create anew "Car" instance as requested
+
+    // Show details about car order
+    // - Check if order is for Used or New car, then check the roof type
+    // - Print details for New or Used car based on roof type
+    if miles != 0 {
+        if roof == true {
+            println!("Prepare a used car: {:?}, {}, Hard top, {} miles\n", motor, color, miles);
+        }
+        else {
+            println!("Prepare a used car: {:?}, {}, Convertible, {} miles\n", motor, color, miles);
+        }
+    }
+    else {
+        if roof == true {
+            println!("Build a new car: {:?}, {}, Hard top, {} miles\n", motor, color, miles);
+        }
+        else {
+            println!("Build a new car: {:?}, {}, Convertible, {} miles\n", motor, color, miles);
+        }
+    }
+
+    // Create a new "Car" instance as requested
     // - Bind first three fields to values of input arguments
     // - "age" field calls "car_quality" function with "miles" input argument
     Car {
@@ -65,11 +85,12 @@ fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Ca
 // Create a tuple for the car quality with the Age ("New or "Used") and mileage
 // Return a tuple with the arrow '->' syntax
 fn car_quality (miles: u32) -> (Age, u32) {
-
-    // Declare and initialize the return tuple value
-    // For a new car, set the miles to 0
-    let quality: (Age, u32) = (Age::New, miles);
-
-    // Return the completed tuple to the caller
-    return quality;
+    if miles == 0 {
+        let quality: (Age, u32) = (Age::New, miles);
+        return quality;
+    }
+    else {
+        let quality: (Age, u32) = (Age::Used, miles);
+        return quality;
+    }
 }
