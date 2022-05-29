8e0e2cd6c2724fec8cedd06e5bb7cfaaefe73da2
Aaron Rothstein <aaron.rothstein@wunita.com>
2022-05-28 20:25:44 -0500

    Completed modules/crates exercise.

---
+++ exercise_10/src/main.rs
@@ -0,0 +1,11 @@
+// https://docs.microsoft.com/en-us/learn/modules/rust-modules-packages-crates/5-exercise-visibility
+
+mod car_factory {
+    pub fn build_car() {
+        println!("Honk honk!");
+    }
+}
+
+fn main() {
+    car_factory::build_car();
+}
